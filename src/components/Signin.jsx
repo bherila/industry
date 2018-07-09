b9518f3d29dfe0b96262ba04434d06ccba3c9275
James Don <jdh@pixelandprocessor.com>
2018-07-09 10:42:49 -0700

    will it work

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -11,7 +11,8 @@ export default class Signin extends Component {
 
     return (
       <div className="panel-landing" id="section-1">
-        <h1 className="landing-heading">Publik</h1>
+        <h1 className="landing-heading">the industry</h1>
+        <p>utilizing Bitcoin protocal and blockchain to imbue words with more weight</p>
         <p>Decentralized micro-blogging</p>
         <p className="lead">
           <button
